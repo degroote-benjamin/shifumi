fdcf073906e656ef5474e5a8ba64cfa7c8c553cb
benjamin <degroote.benjamin@gmail.com>
2017-07-07 10:33:22 +0200

    add dom and html with response stock in the balise

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 var compteur = 0;
 var compteura = 0;
-var buttons = document.querySelectorAll("a");
+var img = document.querySelectorAll(".img");
 var tab = ["pierre", "feuille", "ciseaux"]
 
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function() {
+for (let i = 0; i < img.length; i++) {
+    img[i].addEventListener("click", function() {
         var a = tab[i];
-        var robot = buttons[Math.floor(Math.random() * buttons.length)].innerHTML;
-        var resultat;
 
           var random = Math.random() * 3;
           if (random >= 0 && random <= 1) {
@@ -17,26 +15,20 @@ for (let i = 0; i < buttons.length; i++) {
           } else {
             random = "ciseaux"
           }
+
           if (a == "pierre" && random == "ciseaux" || a == "feuille" && random == "pierre" || a == "ciseaux" && random == "feuille") {
             compteura++
-            alert("joueur" + " " + compteura + " point")
+            document.querySelector("#resultat").innerHTML = "Vous avez win"
           } else if (a == "ciseaux" && random == "pierre" || a == "pierre" && random == "feuille" || a == "feuille" && random == "ciseaux") {
             compteur++
-            alert("ordi" + " " + compteur + " point")
+            document.querySelector("#resultat").innerHTML = "Vous avez loose"
           } else if (a == random) {
-            alert("égalité")
+            document.querySelector("#resultat").innerHTML = "egalité"
           }
-                  document.querySelector("#resultat").innerHTML = a + " " + random
+          document.querySelector("#compta").innerHTML = compteura
+          document.querySelector("#compt").innerHTML = compteur
+          document.querySelector("#resultata").innerHTML = a
+          document.querySelector("#resultatb").innerHTML = random
+
     })
 }
-
-
-
-
-
-//
-// if (compteura == 3) {
-//   alert("VOUS AVEZ GAGNER " + compteura + " a " + compteur)
-// } else if (compteur == 3) {
-//   alert("HAHA TA PERDU " + compteur + " a " + compteura)
-// }
